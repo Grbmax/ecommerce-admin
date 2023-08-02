bac0b69ef24855613e23431115b9696edbeacfa7
Gaurav Bhat <42300572+Grbmax@users.noreply.github.com>
2023-08-02 13:18:20 +0530

    Modal Form

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -6,9 +6,14 @@ import { zodResolver } from "@hookform/resolvers/zod"
 
 import { useStoreModalStore } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
-    name: z.string().min(1),
+    name: z.string().min(1, {
+        message: "Required Field"
+    }),
 })
 
 export const StoreModal = () => {
@@ -16,16 +21,53 @@ export const StoreModal = () => {
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
-    })
+        defaultValues: {
+            name: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values);
+        //TODO: CREATE STORE
+    }
 
     return (
-    <Modal 
-    title="Create Store"
-    description="Add a new store to manage produts and categories"
-    isOpen={StoreModal.isOpen}
-    onClose={StoreModal.onClose}
-    >
-        Future Create Store Form
-    </Modal>
+        <Modal
+            title="Create Store"
+            description="Add a new store to manage produts and categories"
+            isOpen={StoreModal.isOpen}
+            onClose={StoreModal.onClose}
+        >
+            <div>
+                <div className="space-y-4 py-2 pb-4">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            {/* Spreading the field prop allows onChange/onBlur to be handled all at a time in Input Tag */}
+                                            <Input placeholder="E-Commerce" {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                                <Button
+                                    variant="outline"
+                                    onClick={StoreModal.onClose}>
+                                    Cancel
+                                </Button>
+                                <Button type="submit">Continue</Button>
+                            </div>
+                        </form>
+                    </Form>
+                </div>
+            </div>
+        </Modal>
     )
 }
